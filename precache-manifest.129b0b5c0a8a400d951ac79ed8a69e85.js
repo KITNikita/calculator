c5709028c7d8189ec5703ad09ea592df8442e8a7
KITNikita <nik.parashchich@gmail.com>
2021-11-22 18:05:44 +0200

    Updates

---
+++ precache-manifest.129b0b5c0a8a400d951ac79ed8a69e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5714fb9d497ec92b24c55f115de9179",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "f4ce4aeaa56fd220aa7f",
+    "url": "/calculator/static/css/main.8b738425.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "f4ce4aeaa56fd220aa7f",
+    "url": "/calculator/static/js/main.7929eb5e.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
